839c6e5d3319e83a27f78a88d87f96a59966f8ea
pattabhia <shivaram2004@gmail.com>
2022-02-18 06:18:57 +0530

    state change in class component

--- hello-world/src/components/WelcomeMeByName.js
+++ hello-world/src/components/WelcomeMeByName.js
@@ -3,11 +3,27 @@ import React, { Component } from "react";
 
 class WelcomeMeByName extends Component {
 
-    render(props) {
+    constructor() {
+        super()
+        this.state = {
+            message: "Login"
+        }
+    }
 
+    subscribe() {
+        this.setState({
+            message: "Subscribed"
+        })
+    }
+
+    render() {
+        //don't need to give props as an argument in class component,
+        //it is optional, giving no harm
         return (
             <div>
                 <h1>Welcome {this.props.name}</h1>
+                <h1>You are on {this.state.message} page</h1>
+                <button onClick={() => this.subscribe()}>Click Me</button>
             </div>
         )
 
